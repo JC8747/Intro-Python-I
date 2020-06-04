ff859c69ea9cdcb752c562ca019f78cf56888dc2
JC8747 <jonathan.chiaverini@gmail.com>
2020-06-03 20:55:08 -0400

    Completed all 15 exercises

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    print(f.read())
+    f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    f.write('Nothing in this world can take the place of persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. Persistence and determination alone are omnipotent. The slogan Press On! has solved and always will solve the problems of the human race. -Calvin Coolidge')
+    f.close()
+
+with open('bar.txt', 'r') as f:
+    print(f.read())
+    f.close()
